d74d26775780a645ab7e30cbcd0bbc61a106e1c7
icelin99 <xlinbing@gmail.com>
2025-05-06 00:05:11 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/mcts_web/' 
-    : '/'
+    ? '/mcts_web/'
+    : '/',
+  outputDir: 'dist',
+  assetsDir: 'storyteller',
+  configureWebpack: {
+    devtool: 'source-map'
+  }
 })
